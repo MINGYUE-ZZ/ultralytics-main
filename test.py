387015ec3649784ef54105f700d6bd06c7c3096f
明月 <2901236054@qq.com>
2024-06-04 17:26:29 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+from pip._internal.vcs import git
+
+from ultralytics import YOLO
+
+# print("hello")
+# yolo = YOLO("./yolov8n.pt",task="detect")
+# result = yolo(source="./ultralytics/assets/bus.jpg",save=True)
+
